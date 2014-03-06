d4b3243c30e0cd2a19650725e07f5d025282dfa8
Kyle DeFrancia <kyle.def@zoho.com>
2014-03-06 15:50:48 -0800

    Change generate password function to use base64 encoding to convert bit array output to a string.  Use a custom character set that replaces some characters with special characters. Generated passwords are 100 characters long.

--- extension/js/password.js
+++ extension/js/password.js
@@ -47,79 +47,38 @@ greystash.generatePassword = function(url, typed, extPass){
     var toHash = url + extPass;
     var pass;
     var attempt = 0;
+    var loopCount = 30000;
     do {
-        // use the typed password as a random salt
-        // 200 = loop count 110 = num bits for key
-        var bitArray = sjcl.misc.pbkdf2(toHash, typed + attempt, 140000, 110);
-        //convert bit array to a hex number
-        var hash = sjcl.codec.hex.fromBits(bitArray);
-        
-        console.log("hashReturned: " + hash);
+        // 600 = number of bits to output. 600 bits == 100 base64 chars
+        var bits = sjcl.misc.pbkdf2(toHash, typed + attempt, loopCount, 600);
 
-        //convert hash to ASCII password
-        pass = greystash.convertBase(hash, BASE16, BASE92);
+        //convert bit array to a string using base64 encoding
+        var b64 = sjcl.codec.base64.fromBits(bits);
+        
+        // 1245679go@wayNSA!+/.$#CDFGHJKLMPQUVWXYBO08IEZbcefhijklmnpqrstvxz
+        // The following replacements are made to the SJCL base64 char set:
+        // @ for 3
+        // $ for T
+        // # for u
+        // ! for R
+        // . for d
+        var subs = {'3':'@', 'T':'$', 'u':'#', 'R':'!', 'd':'.'};
+        pass = b64.replace(/3|T|u|R|d/g,function(match) {return subs[match];});
 
         // truncate if needed
         var max = greystash.getRule(url).max_len;
         if (max && pass.length > max) pass = pass.substring(0, max);
         
-        console.log("- Generated Password: " + pass);
         attempt++;
+        loopCount = 1; // try passwords faster after first attempt
     } while (!greystash.checkRule(url, pass) && (attempt < 100));
-    //100 chosen at random for now 
-    
+
+    console.log(pass ?
+        'genPass: Generated password: ' + pass :
+        'genPass: ERROR: cannot generate a password for ' + url);
+
     return pass;
 }
-
-
-/*
- * convertBase()
- * 
- * Converts a string whose characters are in one base to another base
- * (this doesn't make sense, I don't know exactly what it should say)
- *
- * @param src The string to change the base of
- * @param srctable The set of characters that make up the current base
- * @param desttable  The set of characters that make up the new base
- *
- * @return A string 
- */
-greystash.convertBase = function(src, srctable, desttable){
-    var srclen = srctable.length;
-    var destlen = desttable.length;
-    // first convert to base 10
-    var val = 0;
-    var numlen = src.length;
-    for (var i = 0; i < numlen; i ++)
-    {
-        val = val * srclen + srctable.indexOf(src.charAt(i));
-    }
-    if (val < 0)
-    {
-        return 0;
-    }
-    // then covert to any base
-    var r = val % destlen;
-    var res = desttable.charAt(r);
-    var q = Math.floor(val / destlen);
-    while (q)
-    {
-        r = q % destlen;
-        q = Math.floor(q / destlen);
-        res = desttable.charAt(r) + res;
-    }
-    return res;
-}
-
-var BASE2  = "01";
-var BASE8  = "01234567";
-var BASE10 = "0123456789";
-var BASE16 = "0123456789abcdef";
-var BASE32 = "0123456789abcdefghijklmnopqrstuvwxyz";
-var BASE62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var BASE75 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_.,!=-*(){}[]";
-//Joe Devlin added following line to enable all special characters allowed in passwords
-var BASE92 = "!\"#$%&'()*+'-./0123456789:;<=>?@abcdefghijklmnopqrstuvwxyz[\]^_`ABCDEFGHIJKLMNOPQRSTUVWZYZ";
 
 
 /*
